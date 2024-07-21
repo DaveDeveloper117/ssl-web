fe6b855c8542d6df7bcdf5541ff43b4efdb066f1
David Butrón <dabicho803@gmail.com>
2024-07-21 13:46:42 -0600

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ssl-web';
+export class AppComponent implements OnInit {
+  title = 'webview-example';
+  latitude: number = 0;
+  longitude: number = 0;
+
+  ngOnInit() {
+    // Registrar la función `updateLocation` en el objeto global `window`
+    (window as any).updateLocation = this.updateLocation.bind(this);
+    this.getLocationFromAndroid();
+  }
+
+  getLocationFromAndroid() {
+    if ((window as any).Android) {
+      const location = (window as any).Android.getLocation();
+      if (location) {
+        const [latitude, longitude] = location.split(',');
+        this.updateLocation(parseFloat(latitude), parseFloat(longitude));
+      }
+    } else {
+      console.log('Android interface not available');
+    }
+  }
+
+  updateLocation(latitude: number, longitude: number) {
+    this.latitude = latitude;
+    this.longitude = longitude;
+    console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+  }
 }
